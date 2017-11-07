0590787ff7e3c22cf06c5ace4653aad38009f0a3
Geovanny Fajardo <gfajardo@smartprocure.us>
2017-11-07 14:14:08 -0500

    testing

--- src/GoogleAuth.js
+++ src/GoogleAuth.js
@@ -34,15 +34,12 @@ export default class GoogleAuth extends React.Component {
     } = this.props
 
     await loadScript('google-platform','https://apis.google.com/js/platform.js')
-    window.gapi.load('auth2', () => {
-        if (!gapi.auth2.getAuthInstance()) {
-          gapi.auth2.init({
-            client_id: _.trimEnd(appId, '.apps.googleusercontent.com'),
-            fetch_basic_profile: fetchBasicProfile,
-            scope,
-          })
-        }
-      })    
+    await window.gapi.load('auth2')
+    window.gapi.auth2.init({
+      client_id: _.trimEnd(appId, '.apps.googleusercontent.com'),
+      fetch_basic_profile: fetchBasicProfile,
+      scope,
+    })
   }
 
   async clickHandler() {
