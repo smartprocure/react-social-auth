046f9a8f34ba50eec8cbb9640ba0b75f0558089f
Geovanny Fajardo <gfajardo@smartprocure.us>
2017-11-07 15:41:48 -0500

    testing

--- src/GoogleAuth.js
+++ src/GoogleAuth.js
@@ -1,6 +1,7 @@
 import _ from 'lodash/fp'
 import React from 'react'
 import { loadScript, hasRequiredSettings } from './util/common'
+import bluebird from 'bluebird'
 
 let getAuthPayload = (appId, user) => {
   let profile = user.getBasicProfile()
@@ -33,17 +34,18 @@ export default class GoogleAuth extends React.Component {
       fetchBasicProfile = true,
     } = this.props
 
-    loadScript('google-platform', 'https://apis.google.com/js/platform.js')
-      .then(() => window.gapi.load('auth2', () => {
-        if (!window.gapi.auth2.getAuthInstance()) {
-          window.gapi.auth2.init({
-            client_id: _.trimEnd(appId, '.apps.googleusercontent.com'),
-            fetch_basic_profile: fetchBasicProfile,
-            scope,
-          })
-        }
-      })
-    )
+    loadScript('google-platform', 'https://apis.google.com/js/platform.js').then(() => {
+      let load = bluebird.promisify(window.gapi.load)
+      return load('auth2')
+    }).then(() => {
+      if (!window.gapi.auth2.getAuthInstance()) {
+        window.gapi.auth2.init({
+          client_id: _.trimEnd(appId, '.apps.googleusercontent.com'),
+          fetch_basic_profile: fetchBasicProfile,
+          scope,
+        })
+      }
+    })
   }
 
   clickHandler() {
