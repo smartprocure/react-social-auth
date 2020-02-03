4195ce6cc8488b2fab349cc23de7d42a6b70071c
Geovanny Fajardo <gfajardo@govspend.com>
2020-02-03 00:03:32 -0500

    Cleanup based on review.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,14 @@
 module.exports = {
-	extends: 'smartprocure',
-	parser: 'babel-eslint',
-	parserOptions: {
-		sourceType: 'module'
-	}
+  extends: 'smartprocure',
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      createClass: 'createReactClass',
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
 }
